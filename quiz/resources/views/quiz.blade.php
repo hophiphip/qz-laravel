2ef4016d0909bfa4b0bb6528a2c468e476c54bbb
hophiphip <nighop@yandex.ru>
2022-01-15 23:42:33 +0400

    alpine implementation

--- quiz/resources/views/quiz.blade.php
+++ quiz/resources/views/quiz.blade.php
@@ -17,7 +17,6 @@
 </head>
 
 <!-- TODO: Cypress tests -->
-<!-- TODO: Moar alpine -->
 <!-- TODO: Add question completion status in 'one question per page mode' -->
 
 <body x-data="quiz">
@@ -25,7 +24,7 @@
     @include('shared.gohome')
 
     <!-- Quiz title -->
-    <h1 class="quiz-title">{{ $quiz->getTitle() }}</h1>
+    <h1 class="quiz-title" x-text="quiz.title"></h1>
 
     <!-- Quiz settings -->
     <div class="settings-container">
@@ -46,24 +45,30 @@
 
     <!-- Quiz questions -->
     <section id="quiz-contents" class="quiz-contents">
-        @foreach($quiz->getQuestions() as $key => $question)
-            <section id="{{ $question->getUUID() }}" class="quiz-question" x-show="showAllQuestions || (currentQuestionIdx == {{ $key }})">
-                <h3>Question {{ $key }}</h3>
+        <template x-for="(question, questionIndex) in quiz.questions">
+            <section x-bind:id="question.uuid" class="quiz-question" x-show="showAllQuestions || (currentQuestionIdx == questionIndex)">
+                <h3 x-text="`Question ${questionIndex + 1}`"></h3>
 
                 <fieldset>
-                    <legend>
-                        {{ $question->getText() }}
+                    <legend x-text="question.text">
                     </legend>
 
-                    @foreach($question->getChoices() as $key => $choice)
+                    <template x-for="(choice, choiceIndex) in question.choices">
                         <div>
-                            <input type="checkbox" id="{{ $choice->getUUID() }}" name="choice" value="{{ $choice->getUUID() }}" class="quiz-choice-checkbox">
-                            <label for="{{ $choice->getUUID() }}">{{ $choice->getText() }}</label>
+                            <input type="checkbox" x-bind:id="choice.uuid" name="choice" x-bind:value="choice.uuid" x-bind:checked="choice.isSelected" x-on:click="choice.isSelected = !choice.isSelected">
+                            <label x-bind::for="choice.uuid" x-text="choice.text"></label>
                         </div>
-                    @endforeach
+                    </template>
                 </fieldset>
             </section>
-        @endforeach
+        </template>
+
+        <!-- Quiz completions status -->
+        <div class="quiz-status">
+            <template x-for="(question , questionIndex) in quiz.questions">
+                <button x-show="!showAllQuestions" x-text="questionIndex + 1" :class="{ 'question-status-visited': question.isVisited, 'question-status': !question.isVisited, 'question-status-current': questionIndex === currentQuestionIdx }" x-on:click="currentQuestionIdx = questionIndex; question.isVisited = true;"></button>
+            </template>
+        </div>
     </section>
 
     <!-- Next & prev question button (only visible in `one question per page mode` -->
@@ -83,98 +88,22 @@
 
     <!-- Submit button -->
     <div class="button-container">
-        <button id="quiz-submit-button" onclick="">Submit</button>
+        <button x-on:click="submitAnswers">Submit</button>
     </div>
 
     <div class="result-container">
-        <h3 id="quiz-result">Submit to get quiz results..</h3>
+        <h3 x-text="quizResultContainerText"></h3>
     </div>
-
-    <script>
-        // Submit button for the quiz
-        let submitButton = document.getElementById('quiz-submit-button');
-        // Quiz result text container
-        let quizResultContainer = document.getElementById('quiz-result');
-
-
-        if (submitButton) {
-            submitButton.addEventListener('click', (evt) => {
-                const quizContents = document.getElementById('quiz-contents');
-
-                if (quizContents) {
-                    // Get quiz questions' choices
-                    let answers = [];
-                    const quizQuestions = quizContents.getElementsByClassName('quiz-question');
-
-                    // `for` loop for older browser support
-                    for (let questionIdx = 0; questionIdx < quizQuestions.length; questionIdx++) {
-                        let answer = { questionUUID: quizQuestions[questionIdx].id ?? '', choices: [] };
-
-                        const questionChoices = quizQuestions[questionIdx].getElementsByClassName('quiz-choice-checkbox');
-
-                        for (let choiceIdx = 0; choiceIdx < questionChoices.length; choiceIdx++) {
-                            if (questionChoices[choiceIdx].checked) {
-                                if (questionChoices[choiceIdx].id) {
-                                    answer.choices.push(questionChoices[choiceIdx].id);
-                                }
-                            }
-                        }
-
-                        answers.push(answer);
-                    }
-
-                    // Construct answers
-                    const quizAnswers = {
-                        quizUUID: `{{ $quiz->getUUID() }}`,
-                        answers: answers,
-                    };
-
-                    // Submit quiz answers
-                    fetch('/api/submit', {
-                        method: 'POST',
-                        headers: {
-                            'Content-Type': 'application/json',
-                        },
-                        body: JSON.stringify(quizAnswers),
-                    })
-                    .then(response => response.json())
-                    // Set the quiz result percentage
-                    .then(data => {
-                        // TODO: Here for the debugging, remove later
-                        console.log(data);
-
-                        if (quizResultContainer) {
-                            // 100% Completion is celebrated with confetti !!!
-                            if (data.result === 1) {
-                                confetti({
-                                    spread: 180,
-                                });
-                            }
-
-                            quizResultContainer.innerText = data.result !== undefined ? `Quiz completion rate: ${data.result * 100}%` : 'Could not calculate quiz results!';
-                        }
-                    })
-                    // Handle the quiz validation errors
-                    .catch((err) => {
-                        if (quizResultContainer) {
-                            // Log error
-                            console.error(err);
-                            // Change result container text
-                            quizResultContainer.innerText = 'Could not submit quiz answers!';
-                        }
-                    });
-                }
-            });
-        }
-    </script>
 
     <script>
         document.addEventListener('alpine:init', () => {
             Alpine.data('quiz', () => ({
-                showAllQuestions: true,
+                showAllQuestions: false,
 
                 currentQuestionIdx: 0,
                 questionCount: {{ count($quiz->getQuestions()) }},
+
+                quizResultContainerText: 'Submit to get quiz results..',
 
                 // NOTE: I think it would be better to store questions like that to show their completion status is real time
                 quiz: {
@@ -191,20 +120,20 @@
                                     uuid: `{{ $choice->getUUID() }}`,
                                     text: `{{ $choice->getText() }}`,
 
-                                    // did user select this question
+                                    // was choice selected by the user
                                     isSelected: false,
-
-                                    // did user visit this question
-                                    isVisited: false,
                                 },
                                 @endforeach
                             ],
+
+                            // was question visited by the user (was at least one choice provided)
+                            isVisited: false,
                         },
                         @endforeach
                     ],
                 },
 
-                submitQuestions() {
+                submitAnswers() {
                     // Flat map questions to answers
                     const answers = this.quiz.questions.flatMap(question => [
                         {
@@ -215,19 +144,53 @@
                         }
                     ]);
 
-                    // DO the fetch ...
+                    // Submit quiz answers
+                    fetch('/api/submit', {
+                        method: 'POST',
+                        headers: {
+                            'Content-Type': 'application/json',
+                        },
+                        body: JSON.stringify({
+                            quizUUID: this.quiz.uuid,
+                            answers: answers,
+                        }),
+                    })
+                        .then(response => response.json())
+                        // Set the quiz result percentage
+                        .then(data => {
+                            // 100% Completion is celebrated with confetti !!!
+                            if (data.result === 1) {
+                                confetti({
+                                    spread: 180,
+                                });
+                            }
+
+                            this.quizResultContainerText = data.result !== undefined ? `Quiz completion rate: ${data.result * 100}%` : 'Could not calculate quiz results!';
+                        })
+                        // Handle the quiz validation errors
+                        .catch((err) => {
+                            // Log error
+                            console.error(err);
+                            // Change result container text
+                            this.quizResultContainerText = 'Could not submit quiz answers!';
+                        });
                 },
 
                 nextQuestion() {
                     this.currentQuestionIdx = (this.currentQuestionIdx + 1) % this.questionCount;
+                    this.quiz.questions[this.currentQuestionIdx].isVisited = true;
                 },
 
                 prevQuestion() {
                     this.currentQuestionIdx = (this.currentQuestionIdx - 1 + this.questionCount) % this.questionCount;
+                    this.quiz.questions[this.currentQuestionIdx].isVisited = true;
                 },
 
                 oneQuestion() {
-                    this.showAllQuestions = false;
+                    this.quiz.questions[this.currentQuestionIdx].isVisited = true;
+
+                    // show one question only when question count > 1
+                    this.showAllQuestions = this.quiz.questions.length < 2;
                 },
 
                 allQuestions() {
