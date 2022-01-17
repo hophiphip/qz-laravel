3f152b3f78623c8cf081cc58063c11a728271aec
hophiphip <nighop@yandex.ru>
2022-01-17 14:33:57 +0400

    Removed URL:to

--- quiz/resources/views/index.blade.php
+++ quiz/resources/views/index.blade.php
@@ -23,7 +23,7 @@
 
     <section class="basic-grid">
         @foreach($quizzes as $quiz)
-            <a href="{{ URL::to('/_/' . $quiz->getUUID()) }}">
+            <a href="{{ '/_/' . $quiz->getUUID() }}">
                 <div class="quiz">
                     <div class="quiz-uuid">UUID: {{$quiz->getUUID() }}</div>
                     <div class="quiz-title">{{$quiz->getTitle() }}</div>
@@ -32,7 +32,7 @@
         @endforeach
 
         <!-- Button to add a new quiz -->
-        <a id="new-quiz" href="{{ URL::to('/new') }}">
+        <a id="new-quiz" href="/new">
             <div class="quiz">
                 <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 9v3m0 0v3m0-3h3m-3 0H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z" />
